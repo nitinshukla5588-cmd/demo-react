e19ca787fab803d06fd1d837a128b5865718fe3f
nitinshukla5588-cmd <nitinshukla5588@gmail.com>
2026-03-31 07:38:16 +0000

    day four

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,18 @@
 import './App.css'
-
 function App() {
-  function get(){
-    return "NITIN SHUKLA"
-  }
-  function sum(a,b){
-    return a+b
-  }
-  const name= "JOHN";
-  let x=80
-  let y=50  
-  return (
-    <div>
-      <h1>User variable with jsx</h1>
-      <h2>Value of variable is {name}</h2>
-      <h3> output={x + y}</h3>
-      <h4>my name is {get()}</h4>
-      <h5>Sum is {sum(100, 123)}</h5>
-    </div>   
+    return (
+    <>
+   
+      <button onClick={function call(){alert("WELCOME IN NEW PROJECT")}}> CLICK ME </button>
+      <br></br>
+      <button onClick={function call(){alert("YOU HAVE CLICKED ON BANANA")}}> BANANA </button>
+      <br></br>
+      <button onClick={function call(){alert("YOU HAVE CLICKED ON APPLE")}}> APPLE </button>
+    </>
   )
 }
+<div>
+      <h1>WELCOME IN NEW PROJECT</h1>
+
+</div>
 export default App
