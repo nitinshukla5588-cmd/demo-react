f090ca1979fef99e24c0a1933b678d0b0fa6812f
nitinshukla5588-cmd <nitinshukla5588@gmail.com>
2026-03-31 07:36:27 +0000

    fruits

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 
 function App() {
+  function get(){
+    return "NITIN SHUKLA"
+  }
+  function sum(a,b){
+    return a+b
+  }
   const name= "JOHN";
   let x=80
   let y=50  
   return (
-    <>
+    <div>
       <h1>User variable with jsx</h1>
-      <h3>Value of variable is {name}</h3>
-      <h4> output={x + y}</h4>
-    </>   
+      <h2>Value of variable is {name}</h2>
+      <h3> output={x + y}</h3>
+      <h4>my name is {get()}</h4>
+      <h5>Sum is {sum(100, 123)}</h5>
+    </div>   
   )
 }
 export default App
